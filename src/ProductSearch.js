6ce347f071e668cefbbc859157181a9c84c01099
Juan Herrera <juan.herrera@sovos.com>
2020-11-20 07:12:23 -0300

    React url to heroku

--- src/ProductSearch.js
+++ src/ProductSearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import classes from './ProductSearch.css';
 
 class ProductDisplay extends Component {
     render() {
@@ -14,8 +15,8 @@ class ProductDisplay extends Component {
             <div> Precio : {this.props.product.price}</div>
         </>;
         let div = <div>
-            <div><h2>{this.props.product.brand}-{this.props.product.description}</h2></div>
-            <img src={this.props.product.image} alt="product"/>
+            {this.getProductTitle()}
+            {this.getProductImage()}
 
             {(this.props.product.onSalePrice === this.props.product.price) ?
                 normalPriceDic
@@ -23,6 +24,45 @@ class ProductDisplay extends Component {
                 offertDiv}
         </div>;
         return div;
+    }
+
+    getProductImage() {
+        return <img src={`http://${this.props.product.image}`} alt="product"/>;
+    }
+
+    getProductTitle() {
+        return <div><h2>{this.props.product.brand}-{this.props.product.description}</h2></div>;
+    }
+}
+
+class MainWindow extends Component {
+    render() {
+        return <div>
+            <div class={classes.headerInfo}>
+                <div class={classes.headerWrapper}>
+                    <h1>Challenge : Walmart Product Search</h1>
+                    <input
+                        name="text"
+                        type="text"
+                        placeholder="Buscar"
+                        onChange={this.props.onChange}
+                        value={this.props.value}/>
+                    <button
+                        onClick={this.props.onClick}>Buscar
+                    </button>
+                </div>
+            </div>
+            {this.props.meals.data ?
+                (
+                    <div>
+                        {this.props.meals.data.map(this.props.prop4)}
+                    </div>
+                )
+                :
+                (
+                    <p>Por favor indicar un termino de busqueda</p>
+                )}
+        </div>;
     }
 }
 
@@ -39,7 +79,8 @@ class ProductSearch extends Component {
     }
 
     makeApiCall = searchInput => {
-        const url = `http://localhost:8080/api/v1/products/search`;
+        //const url = `http://localhost:8080/api/v1/products/search`;
+        const url = `https://wps-backend.herokuapp.com/api/v1/products/search`;
         var searchUrl = `${url}?query=${searchInput}`;
         fetch(searchUrl).then(response => {
             return response.json();
@@ -51,28 +92,9 @@ class ProductSearch extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Challenge : Walmart Product Search</h1>
-                <input
-                    name="text"
-                    type="text"
-                    placeholder="Buscar"
-                    onChange = { event => this.handleOnChange(event)}
-                    value = {this.state.searchValue} />
-                <button
-                    onClick={this.handleSearch}>Buscar</button>
-                {this.state.meals.data ?
-                (
-                    <div>
-                    {this.state.meals.data.map((product, index)=>
-                        <ProductDisplay key={index} product={product}/>)}
-                    </div>
-                )
-                :
-                (
-                    <p>Por favor indicar un termino de busqueda</p>
-                )}
-            </div>
+            <MainWindow onChange={event => this.handleOnChange(event)} value={this.state.searchValue}
+                        onClick={this.handleSearch} meals={this.state.meals} prop4={(product, index) =>
+                <ProductDisplay key={index} product={product}/>}/>
         );
     }
 }
